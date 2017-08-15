20670bd07946cb853f2b7457e7b81d988288d93c
Yeremy Turcios <yeremyturcios@gmail.com>
2017-08-15 08:05:16 -0600

    Finished BalancedTrees.

--- src/org/yeremy/hackerrank/datastructures/balancedtrees/BalancedTrees.java
+++ src/org/yeremy/hackerrank/datastructures/balancedtrees/BalancedTrees.java
@@ -2,59 +2,135 @@ package org.yeremy.hackerrank.datastructures.balancedtrees;
 
 public class BalancedTrees
 {
-    private Node root;
-
     static Node insert(Node root, int val)
     {
-        root = insert(root, val, 0);
-        balance(null, root);
+        root = insertNode(root, val);
+        root = balance(null, root, null);
+        root = recalculateHeights(root);
         return root;
     }
 
-    private static void balance(Node parent, Node node)
+    private static Node recalculateHeights(Node node)
     {
-        int balanceFactor = height(node.left) - height(node.right);
-        if (balanceFactor == 2 || balanceFactor == -2)
-        {
-            balance(node, node.left);
-        }
-
-        balance(node, node.right);
-        balance(node, node.left);
+        if (node == null) return null;
+        node.ht = height(node);
+        recalculateHeights(node.left);
+        recalculateHeights(node.right);
+        return node;
     }
 
-    private static void rotate(Node parent, Node node)
+    private static Node balance(Node parent, Node node, Direction direction)
     {
-        Node child;
-        Node grandChild;
-        if (node.left == null) child = node.right;
-        else                   child = node.left;
-
-        if (child.left == null) grandChild = child.right;
-        else                    grandChild = child.left;
-
-        // Left right case
-        if (parent.val > child.val && child.val < grandChild.val)
+        int balanceFactor = (1 + height(node.left)) - (1 + height(node.right));
+        if (balanceFactor > 1 || balanceFactor < -1)
         {
-            parent.left = grandChild;
-            grandChild;
-
+            // Check Left Left case
+            if (node.left != null && node.left.left != null)
+            {
+                node = rotateLeftLeft(parent, node, direction);
+            }
+            // Check Left Right case
+            else if (node.left != null && node.left.right != null)
+            {
+                node = rotateLeftRight(node);
+                node = rotateLeftLeft(parent, node, direction);
+            }
+            // Check Right Right case
+            else if (node.right != null && node.right.right != null)
+            {
+                node = rotateRightRight(parent, node, direction);
+            }
+            // Check Right Left case
+            else if (node.right != null && node.right.left != null)
+            {
+                node = rotateRightLeft(node);
+                node = rotateRightRight(parent, node, direction);
+            }
         }
-
+        return node;
     }
 
-    private static Node insert(Node parent, int val, int height)
+    private static Node rotateLeftLeft(Node parent, Node node, Direction direction)
+    {
+        Node temp = node.left;
+        temp.left = node.left.left;
+        node.left = null;
+        node.right = null;
+        temp.right = node;
+        node = temp;
+
+        if (direction == Direction.RIGHT)
+        {
+            parent.right = node;
+        }
+        else if (direction == Direction.LEFT)
+        {
+            parent.left = node;
+        }
+
+        return node;
+    }
+
+    private static Node rotateLeftRight(Node node)
+    {
+        Node temp = node.left;
+        node.left = node.left.right;
+        node.left.left = temp;
+        node.left.left.left = null;
+        node.left.left.right = null;
+        return node;
+    }
+
+    private static Node rotateRightRight(Node parent, Node node, Direction direction)
+    {
+        Node temp = node.right;
+        temp.right = node.right.right;
+        node.left = null;
+        node.right = null;
+        temp.left = node;
+        node = temp;
+
+        if (direction == Direction.RIGHT)
+        {
+            parent.right = node;
+        }
+        else if (direction == Direction.LEFT)
+        {
+            parent.left = node;
+        }
+
+        return node;
+    }
+
+    private static Node rotateRightLeft(Node node)
+    {
+        Node temp = node.right;
+        node.right = node.right.left;
+        node.right.right = temp;
+        node.right.right.left = null;
+        node.right.right.right = null;
+        return node;
+    }
+
+    private static Node insertNode(Node parent, int val)
     {
         if (parent == null)
         {
             Node child = new Node();
             child.val = val;
-            child.ht = height;
             return child;
         }
 
-        if (val > parent.val)       parent.right = insert(parent.right, val, height + 1);
-        else                        parent.left  = insert(parent.left, val, height + 1);
+        if (val > parent.val)
+        {
+            parent.right = insert(parent.right, val);
+            parent.right = balance(parent, parent.right, Direction.RIGHT);
+        }
+        else
+        {
+            parent.left  = insert(parent.left, val);
+            parent.left = balance(parent, parent.left, Direction.LEFT);
+        }
 
         return parent;
     }
@@ -62,35 +138,33 @@ public class BalancedTrees
 
     private static int height(Node node)
     {
-        if (node == null) return 0;
-        if (node.left == null && node.right == null)
-        {
-            return node.ht;
-        }
-        int leftHeight = height(node.left);
-        int rightHeight = height(node.right);
-
-        if (leftHeight > rightHeight) return leftHeight;
-        else return rightHeight;
+        if (node == null) return -1;
+        return 1 + Math.max(height(node.left), height(node.right));
     }
 
     private static class Node
     {
-        int val;   //Value
-        int ht;      //Height
-        Node left;   //Left child
-        Node right;   //Right child
+        int val;
+        int ht;
+        Node left;
+        Node right;
+    }
+
+    private enum Direction
+    {
+        LEFT, RIGHT;
     }
 
     public static void main(String[] args)
     {
         BalancedTrees balancedTree = new BalancedTrees();
 
-        Node root = balancedTree.insert(null, 5);
-        balancedTree.insert(root, 3);
-        balancedTree.insert(root, 4);
+        Node root = balancedTree.insert(null, 3);
+        root = balancedTree.insert(root, 2);
+        root = balancedTree.insert(root, 4);
+        root = balancedTree.insert(root, 5);
+        root = balancedTree.insert(root, 6);
 
-
-        System.out.println(balancedTree);
+        System.out.println(root);
     }
 }
